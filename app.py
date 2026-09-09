4fe321a3d3e9037ca9357d475b2b63f84457a36f
stsadrian <adriansilvasantos311@gmail.com>
2026-09-09 14:54:31 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ import pandas as pd
 st.set_page_config(page_title="Dashboard Musical", page_icon="🎵", layout="wide")
 
 ## 1.Coloque o titulo do dashboard
-
+st.title('congela mente')
 ## 2.Carregar os dados do arquivo 'Dados_Artistas.csv'
-df = pd.read_csv('Dados_Artistas.csv')
-
+df = pd.read_parquet('Dados_artistas.parquet')
+                                                        
 # Único filtro, adicionando opção 'TODOS'
 artista = st.selectbox("Escolha um Artista:", ['TODOS'] + list(df['Artist'].unique()))
 
 ## 3.Filtrar os dados com base na seleção do artista
-
+if artista == "TODOS":
+    dados = df 
+else:
+    dados = df[df['Artist'] == artista]
 
 # Apenas 3 métricas
 st.write("### 📈 Métricas")
@@ -32,8 +35,8 @@ with container:
     with graf_esq:
         
         if artista == 'TODOS':
-            st.write("#### Artistas Mais Populares no YouTube")
-            top_5 = df['Artist'].value_counts().head(5)
+            st.write("#### Artistas Mais Populares no YouTube") 
+            top_5 = dados['Artist'].value_counts().head(5)
             st.bar_chart(top_5)
         else:
             st.write("#### Músicas Mais Populares no YouTube")
@@ -52,3 +55,4 @@ with container:
 top_10_spotify = dados.sort_values('Stream', ascending=False).head(10)[['Track', 'Artist', 'Stream']]
 
 ## 5. Mostre a tabela que criamos acima
+st.dataframe(top_10_spotify)
